ed5297e11d7f3d0e82a45bb1cffb4ed2c24a0715
Dexileees <vdima4015@gmail.com>
2026-04-01 16:18:00 +0300

    first commit

---
+++ lesson.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+        unsigned char size;
+        int *element;
+    }SETOFINT;
+
+SETOFINT* create_set(int *arr, int n){
+    SETOFINT *set = malloc(sizeof(SETOFINT));
+    set->size = n;
+    set->element = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        set->element[i] = arr[i];
+    }
+    return set;
+}
+
+void free_set(SETOFINT *set) {
+    free(set->element);
+    free(set);
+}
+
+void print_set(SETOFINT *set) {
+    printf("{ ");
+    for (int i = 0; i < set->size; i++) {
+        printf("%d ", set->element[i]);
+    }
+    printf("}\n");
+}
+
+SETOFINT* set_union(SETOFINT *a, SETOFINT *b) {
+    int i = 0, j = 0, k = 0;
+    int *res = malloc((a->size + b->size) * sizeof(int));
+
+    while (i < a->size && j < b->size) {
+        if (a->element[i] < b->element[j])
+            res[k++] = a->element[i++];
+        else if (a->element[i] > b->element[j])
+            res[k++] = b->element[j++];
+        else {
+            res[k++] = a->element[i];
+            i++; j++;
+        }
+    }
+
+    while (i < a->size) res[k++] = a->element[i++];
+    while (j < b->size) res[k++] = b->element[j++];
+
+    SETOFINT *result = malloc(sizeof(SETOFINT));
+    result->size = k;
+    result->element = res;
+    return result;
+}
+
+SETOFINT* set_intersection(SETOFINT *a, SETOFINT *b) {
+    int i = 0, j = 0, k = 0;
+    int *res = malloc((a->size < b->size ? a->size : b->size) * sizeof(int));
+
+    while (i < a->size && j < b->size) {
+        if (a->element[i] < b->element[j])
+            i++;
+        else if (a->element[i] > b->element[j])
+            j++;
+        else {
+            res[k++] = a->element[i];
+            i++; j++;
+        }
+    }
+
+    SETOFINT *result = malloc(sizeof(SETOFINT));
+    result->size = k;
+    result->element = res;
+    return result;
+}
+
+
+
+
+int cmp(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main(){
+    int a_arr[] = {-4, 15, 7, 0, 9};
+    int b_arr[] = {-6, 20, -4, 15, 0, 1, 2, 3};
+
+    SETOFINT *A = create_set(a_arr, 5);
+    SETOFINT *B = create_set(b_arr, 8);
+
+    qsort(A->element, A->size, sizeof(int), cmp);
+    qsort(B->element, B->size, sizeof(int), cmp);
+
+    SETOFINT *u = set_union(A, B);
+    print_set(u);
+
+    free_set(A);
+    free_set(B);
+    free_set(u);
+
+    return 0;
+}
